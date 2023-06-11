b24fc8227d84ea79d4f783d87df73601120023b9
humaiun <humaiunkobirr@gmail.com>
2023-06-11 18:22:47 +0600

    added navbar

---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { FaPlus, FaUserCircle } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <Navbar className="shadow mb-4" collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Button variant="primary">News</Button> Portal
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link href="#deets">
+              <Button variant="danger">
+                <FaPlus /> Advertise
+              </Button>
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              <Button variant="outline-dark">
+                <FaUserCircle />
+              </Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
