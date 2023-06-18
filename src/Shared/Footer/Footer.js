69f4161a06b36141798ffdf9e410f3acdeb17d1a
humaiun <humaiunkobirr@gmail.com>
2023-06-19 02:33:06 +0600

    add footer

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <div>
-      <h1>Copy right 2023</h1>
+    <div className="bg-dark py-5 mt-5">
+      <p className="text-white text-center fs-5">Copyright &copy; {year} Daily News.</p>
     </div>
   );
 };
